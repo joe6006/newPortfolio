720004b29081ee49e2509cf2525e58c6432e5523
Joseph Hill <josephhill155@gmail.com>
2025-12-11 17:11:06 -0600

    projctsections

--- my-react-app/src/components/project.jsx
+++ my-react-app/src/components/project.jsx
@@ -1,18 +1,54 @@
 import React, { useEffect } from 'react'
 import useEmblaCarousel from 'embla-carousel-react'
 import Autoplay from 'embla-carousel-autoplay'
-import './project.module.css'
- function renderProjects() {
-  const [emblaRef] = useEmblaCarousel({ loop: false }, [Autoplay()])
+import './project.css'
+import photo1 from '../assets/home_icon.png'
+ function RenderProjects() {
+  const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay()])
+
+  const projectCard = [
+    {
+      id:1,
+      src:photo1,
+      alt:"project_name",
+    },
+     {
+      id:2,
+      src:photo1,
+      alt:"project_name",
+    },
+     {
+      id:3,
+      src:photo1,
+      alt:"project_name",
+    },
+     {
+      id:4,
+      src:photo1,
+      alt:"project_name",
+    }
+  ]
+
+
 
   return (
-    <div className="embla" ref={emblaRef}>
-      <div className="embla__container">
-        <div className="embla__slide">Slide 1</div>
-        <div className="embla__slide">Slide 2</div>
-        <div className="embla__slide">Slide 3</div>
+    <div className="embla2" ref={emblaRef}>
+       <div className="embla__container2">
+            {projectCard.map((projects)=>{
+                <div className="embla__slide2" key={projects.id}>
+                  <img 
+                                        
+                    src={projects.src} 
+                    alt={projects.alt} 
+                                          
+                    />
+                  </div>
+              })
+            }
+        
       </div>
+ 
     </div>
   )
 }
-export default renderProjects
+export default RenderProjects
