2b1fb24f4e189d2a30280afee1c8b8606ff4f6e5
Joseph Hill <josephhill155@gmail.com>
2025-12-14 14:08:09 -0600

    embla project

--- my-react-app/src/components/project.jsx
+++ my-react-app/src/components/project.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import useEmblaCarousel from 'embla-carousel-react'
 import Autoplay from 'embla-carousel-autoplay'
 import './project.css'
 import photo1 from '../assets/home_icon.png'
- function RenderProjects() {
+
+function RenderProjects() {
   const [emblaRef] = useEmblaCarousel({ loop: true, axis:"y"}, [Autoplay()])
 
   const projectCard = [
@@ -11,48 +12,49 @@ import photo1 from '../assets/home_icon.png'
       id:1,
       src:photo1,
       alt:"project_name",
+      description:"This is project 1, showcasing the home icon."
     },
-     {
+    {
       id:2,
       src:photo1,
       alt:"project_name",
+      description:"This is project 2, another variation of the home icon."
     },
-     {
+    {
       id:3,
       src:photo1,
       alt:"project_name",
+      description:"This is project 3, demonstrating carousel functionality."
     },
-     {
+    {
       id:4,
       src:photo1,
       alt:"project_name",
+      description:"This is project 4, with looping enabled."
     }
   ]
 
-
-
   return (
     <>
-    <div className="embla2" ref={emblaRef}>
-       <div className="embla__container2">
-            {projectCard.map((projects)=>(
-                <div className="embla__slide2" key={projects.id}>
-                  <div className='project-card'>
-                    <img 
-                                          
-                      src={projects.src} 
-                      alt={projects.alt} 
-                                            
-                      />
-                    </div>
-                  </div>
-            ))
-            }
-        
+      <div className="embla2" ref={emblaRef}>
+        <div className="embla__container2">
+          {projectCard.map((projects)=>(
+            <div className="embla__slide2" key={projects.id}>
+              <div className='project-card'>
+                <img 
+                  src={projects.src} 
+                  alt={projects.alt} 
+                />
+                <p className="project-description">
+                  {projects.description}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
- 
-    </div>
-  </>
+    </>
   )
 }
+
 export default RenderProjects
